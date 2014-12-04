99035ec7247cf638ed2b2aea310fd91ec7f47607
servoclay <francisco.reyesg.14@sansano.usm.cl>
2014-12-03 21:50:03 -0300

    se agregan comentarios a seach&play

--- Seach&Play.py
+++ Seach&Play.py
@@ -1,36 +1,43 @@
 # -*- coding: utf-8 -*-
-import spotipy
-import spotify
-import unicodedata
+import spotipy               # Se importa spotipy, esta libreria nos permite hacer busquedas en la base de datos de spotify
+import spotify               # Se importa spotify, esta libreria nos permite construir nuestro cliente ya que nos facilita la reproducion de musica.
+import unicodedata           # Esta libreria tiene como funcion la limpieza de strings de cualquier caracter no permitido en algun titulo 
 
-session = spotify.Session()
-session.login('USER','PASSWORD')
+session = spotify.Session()        #siempre en este programa se debe crear una session, luego el cliente mismo la recordara y hara el tramite mas rapido
+session.login('USER','PASSWORD')   #se nos solicitara nuestro usuario de spotify, ya que sin esta funcion no nos permitiria la reproducion de musica
 while True:
     session.process_events()
     print "Conectando......."
-    if session.connection.state == 1:
+    if session.connection.state == 1:  # cuando se cumpla dicha condicion, efectivamente estaremos conectados y listos para usar el cliente
         break
     
 spotipy = spotipy.Spotify()
 print "\n\n","-----------------------------------------------------------------","\n\n"
-cancion=raw_input("Ingresa el nombre de la cancion que quieres escuchar: ")
+cancion=raw_input("Ingresa el nombre de la cancion que quieres escuchar: ")     # se crea un input cuya funcion es obtener el nombre de la cancion que se quiere escuchar
 print "\n\n"
-results = spotipy.search(cancion, limit=10)
-songs = {}
+results = spotipy.search(cancion, limit=10)   # en este caso se implemento que la busqueda nos entregase solo 10 canciones, obviamente son las con mayores coincidencias                                                
+songs = {}                                    # ordenadas de modo mejor a peor opcion
 cont = 1
 for i in results['tracks']['items']:
     on = unicodedata.normalize("NFKD",i['name']).encode("ascii","ignore")+" de "+ unicodedata.normalize("NFKD",i["artists"][0]["name"]).encode("ascii","ignore")
-    songs[str(cont)] = {"play_song":str(i["uri"]),"name":on,"image":i['album']['images'][1]['url']}
-    print str(cont)+".-"+ i['name'],"de",i["artists"][0]["name"]
-    cont += 1
-play = raw_input("\n\n¿Cual Quieres escuchar?: ")
-audio = spotify.AlsaSink(session)
+    songs[str(cont)] = {"play_song":str(i["uri"]),"name":on,"image":i['album']['images'][1]['url'],"ID":i['name']}
+    print str(cont)+".-"+ i['name'],"de",i["artists"][0]["name"] # en esta parte del codigo mas que nada se obtiene informacion valiosa como el nombre
+    cont += 1                                                     # de la cancion, del artista y su cover de album, se almacena todo en un diccionario 
+    
+play = raw_input("\n\n¿Cual Quieres escuchar?: ")    # este input nuevamente nos preguntara cual cancion queremos escuchar, rellenaremos este campo con el numero de la cancion
+audio = spotify.AlsaSink(session)                    # para poder escuchar las canciones se utiliza ALSA, es un software de sonido presente en sistemas linux
 track = session.get_track(songs[play]["play_song"])
 track.load()
 session.player.load(track)
-session.player.play()
+session.player.play()     # se procede con la reproducion
 print "\n\n","########## Estas escuchando ",songs[play]["name"],"##########"
-print songs[play]["image"]
+# se entrega el nombre de la cancion y el artista a  su vez su respectivo cover 
+
+resource = urllib.urlopen(songs[play]["image"])
+output = open(songs[play]["ID"]+".jpg","wb")
+output.write(resource.read())
+output.close()
 raw_input()
 
+ 
 
